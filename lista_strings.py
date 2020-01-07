1709e0a9b3700f9ac879a420739d82dd1010e989
sergioq7777 <sergioq.7777@gmail.com>
2020-01-07 17:01:55 -0500

    Print

---
+++ lista_strings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+my_string = "Curso de codigo"
+
+
+print(my_string)
+print(my_string[0:9])
+print(my_string[0:9:3])
+# Print since the number  in the first position to nine and jump in two spaces.
+print(my_string[0:9:2])
+# Print the number in reverse
+print(my_string[::-1])
