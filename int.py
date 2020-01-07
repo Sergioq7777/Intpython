3d4d21d9f583c2ed376c7acaf5c76bfc6e69b399
sergioq7777 <sergioq.7777@gmail.com>
2020-01-07 16:10:08 -0500

    suma,resta div etc..

--- int.py
+++ int.py
@@ -1,1 +1,25 @@
-print("Hello world")
+#!/usr/bin/python3
+
+number_one = 27
+number_two = 11
+
+result0 = number_one + number_two
+print("La suma es", result0)
+
+
+result1 = number_one - number_two
+print("La resta es", result1)
+
+
+result2 = number_one / number_two
+print("La div es", result2)
+
+result3 = number_one * number_two
+print("La mul es", result3)
+
+result4 = number_one // number_two
+print("La div es", result4)
+
+result5 = number_one ** number_two
+print("El exponencial es", result5)
+
