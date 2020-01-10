e1144a986a3cd85291d80caa430e150eaf79cee0
sergioq7777 <sergioq.7777@gmail.com>
2020-01-09 19:41:01 -0500

    Repeat to understand

---
+++ 1func.py
@@ -0,0 +1,46 @@
+''' def fact_num(num):
+    _fact = 1
+
+    while num > 0:
+        _fact = _fact * num
+        num -= 1
+    return _fact
+
+res = fact_num(5)
+print(res)
+ '''
+''' 
+def factorial_Num(num):
+    fact_T = 1
+    while num > 0:
+        fact_T = fact_T * num
+        num -= 1
+    return fact_T
+
+ress = factorial_Num(5)
+print(ress)
+ '''
+def factorial_number(num):
+    i = 1
+    while num > 0:
+        i = i * num
+        num -= 1
+    return i
+answer = factorial_number(5)
+print(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
