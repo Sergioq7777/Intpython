27da8f295d4da169da04f0284be3eb19d773382e
sergioq7777 <sergioq.7777@gmail.com>
2020-01-11 16:06:07 -0500

    Call functions

---
+++ 2func.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" def sum(num_1, num_2, num_3):
+    return num_1 + num_2 + num_3
+
+def max(num_1, num_2, num_3 = 15):
+    return num_1 * num_2 + num_3
+
+def mix(num_2, num_3,num_1 = 1):
+    return num_1 / num_2 + num_3
+
+
+resultado = sum(2, 3, 9)
+print(resultado)
+
+resultado = max(2, 3)
+print(resultado)
+
+resultado = mix(2, 3)
+print(resultado) """
+
+""" def multiples_valor():
+    return "string", 1, True, 43.4
+
+string, entero, bolean, flotante = multiples_valor()
+
+print(string)
+print(entero)
+print(bolean)
+print(flotante)
+ """
+
+""" def sum(num_1, num_2):
+    return num_1 + num_2
+def res(num_1, num_2):
+    return num_1 - num_2
+def div(num_1, num_2):
+    return num_1 / num_2
+def multipli(num_1, num_2):
+    return num_1 * num_2
+def multiples_valor():
+    return "string", 1, True, 43.4
+
+mi_variable = multipli
+resultado = mi_variable(6, 6)
+print(resultado) """
+
+def sum(num_1, num_2):
+    return num_1 + num_2
+def res(num_1, num_2):
+    return num_1 - num_2
+def div(num_1, num_2):
+    return num_1 / num_2
+def multipli(num_1, num_2):
+    return num_1 * num_2
+def show_valor( funcion ):
+    print(funcion(7, 7))
+
+mi_variable = multipli
+show_valor( mi_variable)
