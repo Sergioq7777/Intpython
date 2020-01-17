ed6bfaf426ddb03f5c50325d414581034ba8a0aa
sergioq7777 <sergioq.7777@gmail.com>
2020-01-17 13:55:36 -0500

    function anonim to resturn

---
+++ lambda.py
@@ -0,0 +1,10 @@
+''' ADD function with lambda '''
+my_function = lambda num1, num2 : num1 + num2
+ress = my_function(10, 27)
+print(ress)
+
+''' ADD function with lambda '''
+formato = lambda sentencia : "¿{}?".format(sentencia)
+
+ress = formato("Puedes hacer esto una pregunta")
+print(ress)
